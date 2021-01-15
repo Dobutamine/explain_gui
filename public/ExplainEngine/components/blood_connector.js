2a26334da1c5d737a75098e588ad2a19facf5005
dobutamine <tim.antonius@outlook.com>
2021-01-15 23:52:05 +0100

    update

--- public/ExplainEngine/components/blood_connector.js
+++ public/ExplainEngine/components/blood_connector.js
@@ -8,7 +8,7 @@ class BloodConnector {
 
     // declare the state variables which are not in the model definition file
     this.res = 0;
-    this.flow = 0;
+    this._flow = 0;
     this.real_flow = 0;
     this.prev_flow = 0;
     this.average_flow = 0;
@@ -23,8 +23,8 @@ class BloodConnector {
   calcResistance() {
 
     // calculate the flow dependent parts
-    let nonlinear_fac1 = this.r_k1 * this.r_k1_fac * this.flow;
-    let nonlinear_fac2 = this.r_k2 * this.r_k2_fac * Math.pow(this.flow, 2);
+    let nonlinear_fac1 = this.r_k1 * this.r_k1_fac * this._flow;
+    let nonlinear_fac2 = this.r_k2 * this.r_k2_fac * Math.pow(this._flow, 2);
 
 
     if (this.comp1.pres > this.comp2.pres) 
@@ -59,32 +59,32 @@ class BloodConnector {
       // find the flow direction
       if (this.comp1.pres > this.comp2.pres) {
         // calculate the flow with direction from comp1 to comp2
-        this.flow = (this.comp1.pres - this.comp2.pres) / this.res;
+        this._flow = (this.comp1.pres - this.comp2.pres) / this.res;
         // remove blood in liters from comp1
-        this.comp1.volOut(this.flow * this._model["modeling_stepsize"]);
+        this.comp1.volOut(this._flow * this._model["modeling_stepsize"]);
         // add blood in liters to comp2
-        this.comp2.volIn(this.flow * this._model["modeling_stepsize"], this.comp1);
+        this.comp2.volIn(this._flow * this._model["modeling_stepsize"], this.comp1);
         // store the real flow
-        this.real_flow = this.flow;
+        this.real_flow = this._flow;
       } else {
         // if no backflow allowed set the flow to zero
         if (this.no_backflow) {
-          this.flow = 0;
+          this._flow = 0;
           this.real_flow = 0;
         } else {
           // calculate the flow with direction from comp2 to comp1 
-          this.flow =(this.comp2.pres - this.comp1.pres) / this.res;
+          this._flow =(this.comp2.pres - this.comp1.pres) / this.res;
           // add blood to comp1 in liters
-          this.comp1.volIn(this.flow * this._model["modeling_stepsize"], this.comp2);
+          this.comp1.volIn(this._flow * this._model["modeling_stepsize"], this.comp2);
           // remove blood from comp2 in lieters
-          this.comp2.volOut(this.flow * this._model["modeling_stepsize"]);
+          this.comp2.volOut(this._flow * this._model["modeling_stepsize"]);
           // store the real flow (flip th sign as the real flow is backwards)
-          this.real_flow = -this.flow;
+          this.real_flow = -this._flow;
         }
       }
       this.average_flow = this.average_flow + this._alpha * (this.real_flow * 60 - this.average_flow);
     } else {
-      this.flow = 0;
+      this._flow = 0;
       this.real_flow = 0;
       this.average_flow = 0;
     }
