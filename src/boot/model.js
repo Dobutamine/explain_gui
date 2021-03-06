e4f9aa28305d1f2af015faa22f6b138a4a97b7e6
dobutamine <tim.antonius@outlook.com>
2021-03-06 19:30:05 +0100

    changed standard model

--- src/boot/model.js
+++ src/boot/model.js
@@ -1,8 +1,8 @@
 import { Model } from 'app/public/model'
 
 // eslint-disable-next-line prefer-const
-// let model = new Model('normal_neonate')
-const model = new Model('normal_pregnant')
+let model = new Model('normal_neonate')
+// const model = new Model('normal_pregnant')
 
 export default ({ Vue }) => {
   Vue.prototype.$model = model
